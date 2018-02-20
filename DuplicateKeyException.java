c29a7a05a4326c77d47e2f383ba041f1ff5e69c6
Namrata S <nshah10001@gmail.com>
2018-02-20 10:54:54 -0800

    Create DuplicateKeyException.java

---
+++ DuplicateKeyException.java
@@ -0,0 +1,11 @@
+
+//-----------------------------
+//DuplicateKeyException.java
+//-----------------------------
+
+public class DuplicateKeyException extends RuntimeException {
+     public DuplicateKeyException(String s) {
+          super(s);
+     }
+}
+
